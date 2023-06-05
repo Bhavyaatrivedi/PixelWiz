061de695e083bdef86e5251cb6dc7a77e53ea662
Bhavyaa Trivedi <bhavyaatrivedi2002@gmail.com>
2023-06-05 20:13:44 +0530

    components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,15 @@ const App = () => {
       Create
     </Link>
     </header>
+    <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
+    <Routes>
+      <Route path='/' element={<Home/>}
+      />
+      <Route path='/craete-post' element={<CreatePost/>}
+      />
+    </Routes>
+
+    </main>
     </BrowserRouter>
   )
 }
