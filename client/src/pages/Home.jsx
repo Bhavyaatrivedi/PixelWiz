00c858ccd8628bbfb2311a9542171864a9b5498d
Bhavyaa Trivedi <bhavyaatrivedi2002@gmail.com>
2023-06-05 20:46:05 +0530

    search text

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,24 @@
 import React, {useState,useEffect} from 'react';
 import { Loader, FormField,Card } from '../components';
 
+const renderCards=({data,title})=>{
+  if(data?.length>0){
+    return data.map((post) =>
+  <Card key={post.id} {...post}> </Card>
+)}
+
+  return (
+    <h2 className='mt-5 font-bold text-[#6449ff] text-xl uppercase'>
+        {title}
+    </h2>
+  )
+  
+}
+
 const Home = () => {
     const [loading, setLoading] = useState(false);
     const [allPosts, setAllPosts]= useState(null);
-    const [searchText, setSearchtext]= useState('abs');
+    const [searchText, setSearchtext]= useState('');
   return (
     <section className='max-w-7xl mx-auto'>
         <div>
@@ -32,12 +46,24 @@ const Home = () => {
                 <>
                 {searchText && (
                     <h2 className='font-medium text-[#666e75] text-xl mb-3'>
-                        Showing results for 
-                        <span className='text-[#222328'>
+                        Showing results for <span className='text-[#222328'>
                             {searchText}
                         </span>
                     </h2>
                 )}
+                <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
+                    {searchText ?(
+                        <renderCards data={[]}
+                        title="No search results found"
+                        ></renderCards>
+                    ):(
+                    <renderCards 
+                    data={[]}
+                    title="No posts found"
+                    />)
+
+                    }
+                </div>
                 </>
             )}
         </div>
